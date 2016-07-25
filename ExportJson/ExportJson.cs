0d54f1bbe512986e68bbdf3bed818583f80d431e
GuoXiaoBo <ambitiongxb@foxmail.com>
2016-07-25 14:50:44 +0800

    支持字段为空值

--- ExportJson/ExportJson.cs
+++ ExportJson/ExportJson.cs
@@ -69,14 +69,14 @@ namespace ExportJsonPlugin
             }
             List<string> _Keys = new List<string>();
 
-            List<Excel.Range> keyRang = GetLine(activeWorksheet, 1);
+            List<string> keyRang = GetLine(activeWorksheet, 1);
             if (keyRang.Count == 0)
             {
                 return;
             }
-            foreach (Excel.Range cell in keyRang)
+            foreach (string cell in keyRang)
             {
-                _Keys.Add(cell.Text);
+                _Keys.Add(cell);
             }
             if (_Keys.Count == 0)
             {
@@ -90,7 +90,7 @@ namespace ExportJsonPlugin
             int index = 1;
             while (true)
             {
-                List<Excel.Range> dataRang = GetLine(activeWorksheet, ++index);
+                List<string> dataRang = GetLine(activeWorksheet, ++index);
                 if (dataRang.Count == 0)
                 {
                     break;
@@ -102,7 +102,7 @@ namespace ExportJsonPlugin
                     stringBuilder.Append("\"" + _Keys[i] + "\":");
                     if (i < dataRang.Count)
                     {
-                        StringBuilder v1 = new StringBuilder(dataRang[i].Text);
+                        StringBuilder v1 = new StringBuilder(dataRang[i]);
                         //string v = dataRang[i].Text;
                         string v = v1.ToString().TrimEnd();
                         bool isInteger = Regex.IsMatch(v, @"^[-]?[1-9]{1}\d*$|^[0]{1}$");
@@ -152,6 +152,12 @@ namespace ExportJsonPlugin
             this.Save(json);
         }
 
+
+        //字段类型
+        List<string> typeRang = new List<string>();
+        //字段名称
+        List<string> keyRang = new List<string>();
+
         /// <summary>
         /// 根据类型导出数据
         /// 1.描述
@@ -163,6 +169,9 @@ namespace ExportJsonPlugin
         /// <param name="e"></param>
         private void ExportJsonOfType(object sender, RibbonControlEventArgs e)
         {
+            typeRang.Clear();
+            keyRang.Clear();
+
             Excel.Worksheet activeWorksheet = Globals.ThisAddIn.Application.ActiveSheet;
             if (activeWorksheet == null)
             {
@@ -170,10 +179,13 @@ namespace ExportJsonPlugin
                 return;
             }
             List<string> _Keys = new List<string>();
-            //字段类型
-            List<Excel.Range> typeRang = GetLine(activeWorksheet, 2);
-            //字段名称
-            List<Excel.Range> keyRang = GetLine(activeWorksheet, 3);
+
+            keyRang = GetLine(activeWorksheet, 3);
+
+            typeRang = GetLine(activeWorksheet, 2);
+
+            
+
             if (typeRang.Count != keyRang.Count || typeRang.Count == 0 || keyRang.Count == 0)
             {
                 MessageBox.Show("字段和类型个数不匹配");
@@ -182,9 +194,9 @@ namespace ExportJsonPlugin
             Dictionary<string, string> _FieldsDic = new Dictionary<string, string>();
             for (int i = 0; i < keyRang.Count; ++i)
             {
-                Excel.Range cell = keyRang[i];
-                _FieldsDic.Add(cell.Text, typeRang[i].Text);
-                _Keys.Add(cell.Text);
+                string cell = keyRang[i];
+                _FieldsDic.Add(cell, typeRang[i]);
+                _Keys.Add(cell);
             }
 
             if (_FieldsDic.Count == 0)
@@ -197,7 +209,7 @@ namespace ExportJsonPlugin
             int index = 3;
             while (true)
             {
-                List<Excel.Range> dataRang = GetLine(activeWorksheet, ++index);
+                List<string> dataRang = GetLine(activeWorksheet, ++index,false);
                 if (dataRang.Count == 0)
                 {
                     break;
@@ -207,11 +219,11 @@ namespace ExportJsonPlugin
 
                 for (int i = 0; i < _Keys.Count; ++i)
                 {
-                    stringBuilder.Append("\"" + ((string)keyRang[i].Text).Trim() + "\":");
-                    string fieldType = ((string)typeRang[i].Text).Trim();
+                    stringBuilder.Append("\"" + ((string)keyRang[i]).Trim() + "\":");
+                    string fieldType = ((string)typeRang[i]).Trim();
                     if (i < dataRang.Count)
                     {
-                        StringBuilder v1 = new StringBuilder(dataRang[i].Text);
+                        StringBuilder v1 = new StringBuilder(dataRang[i]);
                         //string v = dataRang[i].Text;
                         string v = v1.ToString().TrimEnd();
 
@@ -254,7 +266,7 @@ namespace ExportJsonPlugin
                                 break;
                             default:
                                 {
-                                    MessageBox.Show("错误的类型: [ " + fieldType + " ]");
+                                    MessageBox.Show("错误的类型1: [ " + fieldType + " ]");
                                 }
                                 break;
                         }
@@ -276,8 +288,14 @@ namespace ExportJsonPlugin
             this.Save(json);
         }
 
-
-        private List<Excel.Range> GetLine(Excel.Worksheet sheet, int lineNum)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <param name="lineNum"></param>
+        /// <param name="assert">是否判null</param>
+        /// <returns></returns>
+        private List<string> GetLine(Excel.Worksheet sheet, int lineNum,bool assert = true)
         {
             char a = 'A';
             char z = 'Z';
@@ -285,27 +303,67 @@ namespace ExportJsonPlugin
             int col = 1000;
 
             string colStr = "";
-            List<Excel.Range> _Cells = new List<Excel.Range>();
+            List<string> _Cells = new List<string>();
 
             for (int c = 0; c < col; ++c)
             {
                 string rStr = string.Format("{0}{1}", colStr + a, lineNum);
-
                 a++;
-
                 if (a > z)
                 {
                     a = 'A';
                     colStr += a;
                 }
-
                 Excel.Range line = sheet.get_Range(rStr);
-                if (string.IsNullOrEmpty(line.Text))
+                string tempV = line.Text;
+                if (string.IsNullOrEmpty(tempV))
                 {
-                    break;
+                    if(assert || c == typeRang.Count)
+                    {
+                        break;
+                    }
+                    if(c < typeRang.Count)
+                    {
+                        string type = ((string)typeRang[c]).Trim();
+                        switch (type)
+                        {
+                            case "I":
+                            case "F":
+                            case "B":
+                                {
+                                    tempV = "0";
+                                }
+                                break;
+                            case "S":
+                                {
+                                    tempV = "";
+                                }
+                                break;
+                            default:
+                                {
+                                    MessageBox.Show("错误的类型2: [ " + type + " ]");
+                                }
+                                break;
+                        }
+                    }
                 }
-                _Cells.Add(line);
+                _Cells.Add(tempV);
             }
+
+            int index = 0;
+            List<string> tempDataRang = _Cells.FindAll((string s) =>
+            {
+                if (string.IsNullOrEmpty(s) || s.Equals("0"))
+                {
+                    if (++index == _Cells.Count)
+                    {
+                        _Cells.Clear();
+                    }
+                    return true;
+                }
+                
+                return false;
+            });
             return _Cells;
         }
     }
